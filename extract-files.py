f1ce25523a53a27575ad7427c6fdd82cf1dce2d1
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-03-19 20:38:25 -0300

    houji: Address missing SetTaskProfiles symbol in camera libs
    The camera HAL forced stop because it could not load
    /vendor/lib64/hw/camera.xiaomi.so which is due to the missing
    `SetTaskProfiles` symbol referenced in several libraries:
    
    fiqri@Fiqri:~/proprietary_vendor_xiaomi_marble$ grep -i -r SetTaskProfiles
    grep: proprietary/vendor/lib64/hw/com.qti.chi.override.so: binary file matches
    grep: proprietary/vendor/lib64/libmialgoengine.so: binary file matches
    grep: proprietary/vendor/lib64/libcamxcommonutils.so: binary file matches
    
    This is because DECLS was removed in [1] on Android 15 QPR2, and
    caused SetTaskProfiles() to be hidden, so shim SetTaskProfiles
    with libprocessgroup_shim.
    
    [1]: https://cs.android.com/android/_/android/platform/system/core/+/3e7c17a8e7ad7275ea3a65642608b1c0becb38ad
    
    Change-Id: Ic46aae47bdf6be09736fe89d798cb7017f07cc6c
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -72,6 +72,7 @@ blob_fixups: blob_fixups_user_type = {
         'odm/lib64/libcamxcommonutils.so',
         'vendor/lib64/libcameraopt.so',
     ): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
     (
         'odm/lib64/libsnpe_config.so',
     ): blob_fixup()
@@ -82,12 +83,14 @@ blob_fixups: blob_fixups_user_type = {
         'odm/lib64/hw/com.qti.chi.override.so',
         'odm/lib64/libchifeature2.so',
     ): blob_fixup()
+        .add_needed('libprocessgroup_shim.so')
         .replace_needed(
             'android.hardware.graphics.allocator-V1-ndk.so',
             'android.hardware.graphics.allocator-V2-ndk.so'
         ),
 
     'odm/lib64/hw/camera.xiaomi.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so')
         .replace_needed(
             'android.hardware.graphics.allocator-V1-ndk.so',
             'android.hardware.graphics.allocator-V2-ndk.so'
