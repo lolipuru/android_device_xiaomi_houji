968c77fa4628fa4fa5aeb2bb8408472b1ec63b54
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-06-18 17:07:38 +0800

    houji: Enable elf checks for some camera libs
    Reference from https://review.lineageos.org/c/LineageOS/android_tools_extract-utils/+/411165
    
    Now we can clean the version of the symbol on the blob, this allows
    the elf to be checked again on some camera libraries. The libalLDC.so,
    libalhLDC.so, libMiVideoFilter.so and libTrueSight.so blobs in the camera library that
    previously depended on libnativewindow have now been freed by clearing
    the symbol version AHardwareBuffer_* which looks like this:
    
    strings libalLDC.so | grep AHardwareBuffer_
    AHardwareBuffer_allocate
    AHardwareBuffer_describe
    AHardwareBuffer_lock
    AHardwareBuffer_release
    AHardwareBuffer_unlock
    
    strings libalhLDC.so | grep AHardwareBuffer_
    AHardwareBuffer_allocate
    AHardwareBuffer_describe
    AHardwareBuffer_lock
    AHardwareBuffer_release
    AHardwareBuffer_unlock
    
    strings libTrueSight.so | grep AHardwareBuffer_
    AHardwareBuffer_allocate
    AHardwareBuffer_describe
    AHardwareBuffer_isSupported
    AHardwareBuffer_lock
    AHardwareBuffer_release
    AHardwareBuffer_unlock
    AHardwareBuffer_lockPlanes
    
    strings libMiVideoFilter.so | grep AHardwareBuffer_
    AHardwareBuffer_allocate
    AHardwareBuffer_describe
    AHardwareBuffer_lockPlanes
    AHardwareBuffer_release
    AHardwareBuffer_unlock
    
    strings libmorpho_ubwc.so | grep AHardwareBuffer_
    AHardwareBuffer_describe
    
    Change-Id: I16619cf32c4486c7600ef32ab928fb2d8c5cd5e5
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -102,14 +102,20 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .replace_needed('android.hardware.camera.device-V1-ndk.so', 'android.hardware.camera.device-V2-ndk.so'),
     (
+        'odm/lib64/libAncHumanVideoBokehV4.so',
+        'odm/lib64/libTrueSight.so',
         'odm/lib64/libMiPhotoFilter.so',
+        'odm/lib64/libmorpho_ubwc.so',
+        'odm/lib64/libalAILDC.so',
+        'odm/lib64/libalLDC.so',
     ): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
         .clear_symbol_version('AHardwareBuffer_isSupported')
         .clear_symbol_version('AHardwareBuffer_lockPlanes')
         .clear_symbol_version('AHardwareBuffer_release')
-        .clear_symbol_version('AHardwareBuffer_unlock'),
+        .clear_symbol_version('AHardwareBuffer_unlock')
+        .clear_symbol_version('AHardwareBuffer_lock'),
 }
 
 module = ExtractUtilsModule(
