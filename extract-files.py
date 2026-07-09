20eb6bdda0487fc44cb6d569661b0f0e8f62f0d3
lolipuru <banstellazz@gmail.com>
2026-07-09 19:00:38 +0000

    houji: enable ELF checks for all camera libs
    except libmidebuggerdimpl_vendor as it require menureachable, which is GNU target
    
    Change-Id: If5a325ce0f6da81213884c42006dc163b04481ef
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -61,16 +61,6 @@ def blob_fixup_graphic_buffer_size(
                 f.seek(int(offset[:-1], 16))
                 f.write(b'\x00\xa6\x81\x52')  # AArch64 mov w0, #0xd30
 
-def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
-    return f'{lib}-{partition}' if partition == 'vendor' else None
-
-lib_fixups: lib_fixups_user_type = {
-    (
-        'android.hardware.graphics.composer3-V1-ndk',
-        'android.hardware.graphics.allocator-V1-ndk',
-    ): lib_fixup_remove,
-}
-
 blob_fixups: blob_fixups_user_type = {
     (
         'odm/etc/camera/enhance_motiontuning.xml',
@@ -81,10 +71,31 @@ blob_fixups: blob_fixups_user_type = {
     (
         'odm/lib64/libcamxcommonutils.so',
         'vendor/lib64/libcameraopt.so',
-        'odm/lib64/hw/camera.qcom.so'
     ): blob_fixup()
-        .add_needed('libprocessgroup_shim.so'),
+    (
+        'odm/lib64/libsnpe_config.so',
+    ): blob_fixup()
+        .add_needed('liblog.so'),
+
+    (
+        'odm/lib64/hw/camera.qcom.so',
+        'odm/lib64/hw/com.qti.chi.override.so',
+        'odm/lib64/libchifeature2.so',
+    ): blob_fixup()
+        .replace_needed(
+            'android.hardware.graphics.allocator-V1-ndk.so',
+            'android.hardware.graphics.allocator-V2-ndk.so'
+        ),
+
     'odm/lib64/hw/camera.xiaomi.so': blob_fixup()
+        .replace_needed(
+            'android.hardware.graphics.allocator-V1-ndk.so',
+            'android.hardware.graphics.allocator-V2-ndk.so'
+        )
+        .replace_needed(
+            'libtinyxml2.so',
+            'libtinyxml2-v34.so'
+        )
         .replace_needed('libui.so', 'libui-v34.so')
         .call(
             blob_fixup_graphic_buffer_size,
@@ -94,7 +105,7 @@ blob_fixups: blob_fixups_user_type = {
                 '_ZN5mihal9GraBufferC2EjjimPK13native_handle',
                 '_ZN5mihal9GraBufferC2EPKNS_6StreamEPK13native_handle',
             ],
-    ),
+        ),
     (
         'vendor/lib64/vendor.xiaomi.hardware.camera.injection-service.so',
         'vendor/lib64/vendor.xiaomi.hardware.camera.injection-V1-ndk.so',
@@ -116,13 +127,149 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock')
         .clear_symbol_version('AHardwareBuffer_lock'),
+    (
+        'odm/lib64/camera/plugins/com.xiaomi.plugin.anchor.so',
+    ): blob_fixup()
+        .replace_needed(
+            'libtinyxml2.so',
+            'libtinyxml2-v34.so'
+        ),
+    'odm/lib64/com.qti.feature2.anchorsync.so': blob_fixup()
+        .replace_needed(
+            'android.hardware.graphics.allocator-V1-ndk.so',
+            'android.hardware.graphics.allocator-V2-ndk.so'
+        )
+        .replace_needed(
+            'libtinyxml2.so',
+            'libtinyxml2-v34.so'
+        ),
+    (
+        'odm/lib64/camera/com.qti.actuator.houji_ofilm_s5kjn1_gt9764v_tele_actuator.so',
+        'odm/lib64/camera/com.qti.actuator.houji_sunny_ovx9000_ak7316_wide_actuator.so',
+        'odm/lib64/camera/com.qti.actuator.houji_sunny_ovx9000_ak7316_wide_ii_actuator.so',
+        'odm/lib64/camera/com.qti.actuator.houji_sunny_s5kjn1_gt9764v_tele_ii_actuator.so',
+        'odm/lib64/camera/com.qti.eeprom.houji_ofilm_s5kjn1_gt24p128f_tele_eeprom.so',
+        'odm/lib64/camera/com.qti.eeprom.houji_sunny_ov32b_p24c64f_front_eeprom.so',
+        'odm/lib64/camera/com.qti.eeprom.houji_sunny_ovx9000_p24c128e_wide_eeprom.so',
+        'odm/lib64/camera/com.qti.eeprom.houji_sunny_ovx9000_p24c128e_wide_ii_eeprom.so',
+        'odm/lib64/camera/com.qti.eeprom.houji_sunny_s5kjn1_gt24p128f_ultra_eeprom.so',
+        'odm/lib64/camera/com.qti.eeprom.houji_sunny_s5kjn1_p24c128f_tele_ii_eeprom.so',
+        'odm/lib64/camera/com.qti.ois.houji_s5kjn1_bu24618_ois_ii.so',
+        'odm/lib64/camera/com.qti.ois.houji_s5kjn1_bu24618_ois.so',
+        'odm/lib64/camera/com.qti.sensor.houji_ofilm_s5kjn1_tele.so',
+        'odm/lib64/camera/com.qti.sensor.houji_sunny_ov32b40_front.so',
+        'odm/lib64/camera/com.qti.sensor.houji_sunny_ovx9000_wide_ii.so',
+        'odm/lib64/camera/com.qti.sensor.houji_sunny_ovx9000_wide.so',
+        'odm/lib64/camera/com.qti.sensor.houji_sunny_s5kjn1_tele_ii.so',
+        'odm/lib64/camera/com.qti.sensor.houji_sunny_s5kjn1_ultra.so',
+        'odm/lib64/camera/components/com.jigan.node.videobokeh.so',
+        'odm/lib64/camera/components/com.mi.node.aiasd.so',
+        'odm/lib64/camera/components/com.mi.node.rearvideo.so',
+        'odm/lib64/camera/components/com.mi.node.skinbeautifier.so',
+        'odm/lib64/camera/components/com.mi.node.videobokeh.so',
+        'odm/lib64/camera/components/com.mi.node.videofilter.so',
+        'odm/lib64/camera/components/com.mi.node.videonight.so',
+        'odm/lib64/camera/components/com.qti.node.aon.so',
+        'odm/lib64/camera/components/com.qti.node.depth.so',
+        'odm/lib64/camera/components/com.qti.node.depthprovider.so',
+        'odm/lib64/camera/components/com.qti.node.dewarp.so',
+        'odm/lib64/camera/components/com.qti.node.eisv2.so',
+        'odm/lib64/camera/components/com.qti.node.eisv3.so',
+        'odm/lib64/camera/components/com.qti.node.evadepth.so',
+        'odm/lib64/camera/components/com.qti.node.gme.so',
+        'odm/lib64/camera/components/com.qti.node.gyrornn.so',
+        'odm/lib64/camera/components/com.qti.node.hdr10pgen.so',
+        'odm/lib64/camera/components/com.qti.node.hdr10phist.so',
+        'odm/lib64/camera/components/com.qti.node.itofpreprocess.so',
+        'odm/lib64/camera/components/com.qti.node.ml.so',
+        'odm/lib64/camera/components/com.qti.node.mlinference.so',
+        'odm/lib64/camera/components/com.qti.node.pixelstats.so',
+        'odm/lib64/camera/components/com.qti.node.seg.so',
+        'odm/lib64/camera/components/com.qti.node.swec.so',
+        'odm/lib64/camera/components/com.qti.node.swregistration.so',
+        'odm/lib64/camera/components/com.qti.node.swvrt.so',
+        'odm/lib64/camera/components/com.qti.stats.cnndriver.so',
+        'odm/lib64/camera/components/com.xiaomi.node.smooth_transition.so',
+        'odm/lib64/camera/components/libcamxevainterface.so',
+        'odm/lib64/camera/components/libdepthmapwrapper_itof.so',
+        'odm/lib64/camera/components/libdepthmapwrapper_secure.so',
+        'odm/lib64/camera/libchxlogicalcameratable.so',
+        'odm/lib64/com.qti.camx.chiiqutils.so',
+        'odm/lib64/com.qti.chiusecaseselector.so',
+        'odm/lib64/com.qti.feature2.afbrckt.so',
+        'odm/lib64/com.qti.feature2.demux.so',
+        'odm/lib64/com.qti.feature2.derivedoffline.so',
+        'odm/lib64/com.qti.feature2.fusion.so',
+        'odm/lib64/com.qti.feature2.generic.so',
+        'odm/lib64/com.qti.feature2.gs.sm8650.so',
+        'odm/lib64/com.qti.feature2.hdr.so',
+        'odm/lib64/com.qti.feature2.mcreprocrt.so',
+        'odm/lib64/com.qti.feature2.memcpy.so',
+        'odm/lib64/com.qti.feature2.metadataserializer.so',
+        'odm/lib64/com.qti.feature2.mfsr.so',
+        'odm/lib64/com.qti.feature2.ml.so',
+        'odm/lib64/com.qti.feature2.mux.so',
+        'odm/lib64/com.qti.feature2.offlinestatsregeneration.so',
+        'odm/lib64/com.qti.feature2.qcfa.so',
+        'odm/lib64/com.qti.feature2.rawhdr.so',
+        'odm/lib64/com.qti.feature2.realtimeserializer.so',
+        'odm/lib64/com.qti.feature2.rt.so',
+        'odm/lib64/com.qti.feature2.rtmcx.so',
+        'odm/lib64/com.qti.feature2.serializer.so',
+        'odm/lib64/com.qti.feature2.statsregeneration.so',
+        'odm/lib64/com.qti.feature2.stub.so',
+        'odm/lib64/com.qti.feature2.swmf.so',
+        'odm/lib64/com.qti.qseeutils.so',
+        'odm/lib64/com.qualcomm.mcx.distortionmapper.so',
+        'odm/lib64/com.qualcomm.mcx.linearmapper.so',
+        'odm/lib64/com.qualcomm.mcx.nonlinearmapper.so',
+        'odm/lib64/com.qualcomm.mcx.policy.mfl.so',
+        'odm/lib64/com.qualcomm.qti.mcx.usecase.extension.so',
+        'odm/lib64/com.xiaomi.camx.hook.so',
+        'odm/lib64/com.xiaomi.chi.hook.so',
+        'odm/lib64/hw/camera.qcom.sm8650.so',
+        'odm/lib64/hw/com.qti.chi.offline.so',
+        'odm/lib64/libcamerapostproc.so',
+        'odm/lib64/libcamxhwnodecontext.so',
+        'odm/lib64/libcamxifestriping.so',
+        'odm/lib64/libcamximageformatutils.so',
+        'odm/lib64/libcamxncsdatafactory.so',
+        'odm/lib64/libcom.xiaomi.mawutilsold.so',
+        'odm/lib64/libcommonchiutils.so',
+        'odm/lib64/libfastmessage.so',
+        'odm/lib64/libhme.so',
+        'odm/lib64/libipebpsstriping.so',
+        'odm/lib64/libipebpsstriping170.so',
+        'odm/lib64/libipebpsstriping480.so',
+        'odm/lib64/libisphwsetting.so',
+        'odm/lib64/libjpege.so',
+        'odm/lib64/libmctfengine_stub.so',
+        'odm/lib64/libmfec.so',
+        'odm/lib64/libmmcamera_bestats.so',
+        'odm/lib64/libmmcamera_cac.so',
+        'odm/lib64/libmmcamera_lscv35.so',
+        'odm/lib64/libmmcamera_pdpc.so',
+        'odm/lib64/libofflinefeatureintf.so',
+        'odm/lib64/libopestriping.so',
+        'odm/lib64/libtfestriping.so',
+        'odm/lib64/libubifocus.so',
+        'odm/lib64/vendor.qti.hardware.camera.aon-service-impl.so',
+        'odm/lib64/vendor.qti.hardware.camera.offlinecamera-service-impl.so',
+        'odm/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so',
+        'odm/lib64/libmmcamera_mfnr.so',
+        'odm/lib64/libmmcamera_mfnr_t4.so',
+        'odm/lib64/libtunningmemhook.so',
+    ): blob_fixup()
+        .replace_needed(
+            'android.hardware.graphics.allocator-V1-ndk.so',
+            'android.hardware.graphics.allocator-V2-ndk.so'
+    ),
 }
 
 module = ExtractUtilsModule(
     'houji',
     'xiaomi',
     blob_fixups=blob_fixups,
-    lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
     check_elf=True,
     add_firmware_proprietary_file=True,
