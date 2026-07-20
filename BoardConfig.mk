23691118be4c37dd40986ecd8c9e9edd96e3f522
lolipuru <banstellazz@gmail.com>
2026-07-20 18:00:43 +0000

    houji: opt-out form oss kernel for now
    until LTPO flickering problem is solved
    
    Change-Id: Id151835596d6721e2a41be159888638475ddd02d
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,9 @@ include device/xiaomi/sm8650-common/BoardConfigCommon.mk
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+TARGET_OPT_IN_OSS_KERNEL ?= false
+
+ifneq ($(TARGET_OPT_IN_OSS_KERNEL),true)
 # Dtb/o
 BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbo.img
 BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
@@ -34,6 +37,7 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(KERNEL_PATH)/vendor_dlkm/,$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules) \
     $(call find-copy-subdir-files,*,$(KERNEL_PATH)/vendor_ramdisk/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules) \
     $(call find-copy-subdir-files,*,$(KERNEL_PATH)/system_dlkm/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/lib/modules/6.1.25-android14-11-g9f6af9a6c2cc-ab11205628)
+endif
 
 # OTA
 TARGET_OTA_ASSERT_DEVICE := houji,houjiin
